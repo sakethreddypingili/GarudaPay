2a8540f1e79d91bb7eff07447d8ddb6b2f61bf24
saketh reddy pingili <pingilisakethreddy@gmail.com>
2026-06-20 00:17:57 +0530

    chore: gitignore everything except index.html

--- src/js/landing.js
+++
@@ -1,7 +0,0 @@
-var menuButton = document.getElementById("menuButton");
-var navLinks = document.getElementById("navLinks");
-
-// This opens and closes the mobile navigation menu.
-menuButton.addEventListener("click", function () {
-    navLinks.classList.toggle("show");
-});
